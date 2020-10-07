936f5111875145deb7a61586dbad2c15b65315be
Thet Hmuu <thethmuueinsoe@gmail.com>
2020-10-07 09:28:21 +0630

    Implement Devise in Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :blogs
   root 'blogs#index'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
 end
